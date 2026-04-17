eec9b831d5f69b58008d88a98d82163f60014106
bryce198 <brycekirkland116@gmail.com>
2026-04-17 13:44:29 -0400

    B: added player ship model and rotation function

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
-
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -9,13 +9,22 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH /2
+    y = SCREEN_HEIGHT /2
+    star_ship = Player(x, y)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
         screen.fill("black")
+        star_ship.update(dt)
+        star_ship.draw(screen)
         pygame.display.flip()
+        
 
 
 if __name__ == "__main__":
