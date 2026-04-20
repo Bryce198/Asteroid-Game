b68d29b6e6459d3997061d489255941776163086
bryce198 <brycekirkland116@gmail.com>
2026-04-19 23:34:34 -0400

    H: added split mechanic for asteroids

--- asteroids.py
+++ asteroids.py
@@ -1,6 +1,8 @@
 from circleshape import CircleShape
 import pygame
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
+from logger import log_event
+import random
 pygame.init()
 
 class Asteroid(CircleShape):
@@ -12,3 +14,19 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += (self.velocity * dt)
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            log_event("asteroid_split")
+            rotation = random.uniform(20, 50)
+            first_vector = self.velocity.rotate(rotation)
+            second_vector = self.velocity.rotate(-rotation)
+            old_radius = self.radius
+            new_radius = old_radius - ASTEROID_MIN_RADIUS
+            new_asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+            new_asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+            new_asteroid1.velocity = first_vector * 1.2
+            new_asteroid2.velocity = second_vector * 1.2
